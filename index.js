28a51cc4d9db344a633bf962b6f4d684c2d2710a
gerritr56 <68347062+gerritr56@users.noreply.github.com>
2020-11-18 16:35:29 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let age = 30;
 const name = 'Bram';
-const totalAmount = 120;
+const totalAmount = 60;
 
 if (age >= 18) {
     console.log('Jouw leeftijd is 18 of ouder, jij mag naar binnen')
@@ -16,14 +16,11 @@ if (name == 'Sarah' || name == 'Bram') {
     console.log('Je krijgt een gratis biertje')
 }
 
-if (totalAmount > 25 && totalAmount <=50) {
-    console.log('Je krijgt gratis Bitterballen')
-}
 
-if (totalAmount > 50 && totalAmount <=100) {
-    console.log('Je krijgt gratis Nachos')
-}
-
-if (totalAmount > 100) {
+if (totalAmount >= 100) {
     console.log('Je krijgt gratis champagne')
+} else if (totalAmount > 50) {
+    console.log('Je krijgt gratis Nachos')
+} else if (totalAmount > 25) {
+    console.log('Je krijgt gratis Bitterballen')
 }
